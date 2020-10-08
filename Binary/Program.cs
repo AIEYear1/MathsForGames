3fd06ba0bc57f100099551a382c17f1eb3fccb7b
Jarod <j4r0dhub3r@gmail.com>
2020-10-08 11:31:36 -0700

    added more text to the writelines to give context

--- Binary/Program.cs
+++ Binary/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.ComponentModel;
 
 namespace Binary
 {
@@ -8,52 +7,52 @@ namespace Binary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DecToBin(1, 8));
-            Console.WriteLine(DecToBin(42));
-            Console.WriteLine(DecToBin(256));
-            Console.WriteLine(DecToBin(4294967296));
+            Console.WriteLine("Decimal: 1 to Binary: " + DecToBin(1));
+            Console.WriteLine("Decimal: 42 to Binary: " + DecToBin(42));
+            Console.WriteLine("Decimal: 256 to Binary: " + DecToBin(256));
+            Console.WriteLine("Decimal: 4294967296 to Binary: " + DecToBin(4294967296));
             Console.WriteLine();
-            Console.WriteLine(BinToDec("010000000"));
-            Console.WriteLine(BinToDec("010101010"));
-            Console.WriteLine(BinToDec("011110000"));
-            Console.WriteLine(BinToDec("011001100"));
+            Console.WriteLine("Binary: 010000000 to Decimal: " + BinToDec("010000000"));
+            Console.WriteLine("Binary: 010101010 to Decimal: " + BinToDec("010101010"));
+            Console.WriteLine("Binary: 011110000 to Decimal: " + BinToDec("011110000"));
+            Console.WriteLine("Binary: 011001100 to Decimal: " + BinToDec("011001100"));
             Console.WriteLine();
-            Console.WriteLine(BinAddition("111", "111"));
-            Console.WriteLine(BinAddition("1010", "1010"));
-            Console.WriteLine(BinSubtraction("01101", "011", 8));
-            Console.WriteLine(BinSubtraction("010001", "0100", 8));
-            Console.WriteLine(BinMultiplication("101", "10"));
-            Console.WriteLine(BinMultiplication("1011", "11"));
-            Console.WriteLine(BinDivision("01101", "011"));
+            Console.WriteLine("0111 + 0111 = " + BinAddition("111", "111"));
+            Console.WriteLine("01010 + 01010 = " + BinAddition("1010", "1010"));
+            Console.WriteLine("01101 - 011 = " + BinSubtraction("01101", "011", 8));
+            Console.WriteLine("010001 - 0100 = " + BinSubtraction("010001", "0100", 8));
+            Console.WriteLine("0101 * 010 = " + BinMultiplication("101", "10"));
+            Console.WriteLine("01011 * 011 = " + BinMultiplication("1011", "11"));
+            Console.WriteLine("01101 / 011 = " + BinDivision("01101", "011"));
             Console.WriteLine();
-            Console.WriteLine(BinToDec("10000000"));
-            Console.WriteLine(BinToDec("10101010"));
-            Console.WriteLine(BinToDec("11110000"));
-            Console.WriteLine(BinToDec("11001100"));
-            Console.WriteLine(DecToBin(-16));
-            Console.WriteLine(DecToBin(-128));
-            Console.WriteLine(DecToBin(128));
-            Console.WriteLine(DecToBin(-123));
+            Console.WriteLine("Binary: 10000000 to Decimal: " + BinToDec("10000000"));
+            Console.WriteLine("Binary: 10101010 to Decimal: " + BinToDec("10101010"));
+            Console.WriteLine("Binary: 11110000 to Decimal: " + BinToDec("11110000"));
+            Console.WriteLine("Binary: 11001100 to Decimal: " + BinToDec("11001100"));
+            Console.WriteLine("Decimal: -16 to Binary: " + DecToBin(-16));
+            Console.WriteLine("Decimal: -128 to Binary: " + DecToBin(-128));
+            Console.WriteLine("Decimal: 128 to Binary: " + DecToBin(128));
+            Console.WriteLine("Decimal: -123 to Binary: " + DecToBin(-123));
             Console.WriteLine();
-            Console.WriteLine(BinOr("11111", "11111"));
-            Console.WriteLine(BinXor("11111", "11111"));
-            Console.WriteLine(BinAnd("10101", "11111"));
-            Console.WriteLine(BinOr("10101", "11111"));
-            Console.WriteLine(BinXor("00000", "11111"));
-            Console.WriteLine(LeftShift("01", 3));
-            Console.WriteLine(RightShift("0100", 2));
-            Console.WriteLine(BinNot("10101"));
-            Console.WriteLine(LeftShift("0100", 1));
-            Console.WriteLine(RightShift("1010", 1));
-            Console.WriteLine(BinNot("11111"));
+            Console.WriteLine("11111 | 11111 = " + BinOr("11111", "11111"));
+            Console.WriteLine("11111 ^ 11111 = " + BinXor("11111", "11111"));
+            Console.WriteLine("10101 & 11111 = " + BinAnd("10101", "11111"));
+            Console.WriteLine("10101 | 11111 = " + BinOr("10101", "11111"));
+            Console.WriteLine("00000 ^ 11111 = " + BinXor("00000", "11111"));
+            Console.WriteLine("01 << 3 = " + LeftShift("01", 3));
+            Console.WriteLine("0100 >> 2 = " + RightShift("0100", 2));
+            Console.WriteLine("~10101 = " + BinNot("10101"));
+            Console.WriteLine("0100 << 1 = " + LeftShift("0100", 1));
+            Console.WriteLine("1010>>1 = " + RightShift("1010", 1));
+            Console.WriteLine("~11111 = " + BinNot("11111"));
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine(IsLeftMostBitSet(32));
-            Console.WriteLine(IsRightMostBitSet(25));
-            Console.WriteLine(IsBitSet(13, 2));
-            Console.WriteLine(GetRightMostSetBit(40));
+            Console.WriteLine("Is the left most bit of 32 set? " + IsLeftMostBitSet(32));
+            Console.WriteLine("Is the right most bit of 25 set? " + IsRightMostBitSet(25));
+            Console.WriteLine("Is bit 2 of 13 set? " + IsBitSet(13, 2));
+            Console.WriteLine("The right most set bit of 40 is " + GetRightMostSetBit(40));
             PrintBinary(12);
-            Console.WriteLine(IsPowerOfTwo(4));
+            Console.WriteLine("is 4 a power of two? " + IsPowerOfTwo(4));
         }
         #region My Binary shiz that makes my soul hurt
         static string DecToBin(long dec)
@@ -429,7 +428,7 @@ namespace Binary
         }
         static void PrintBinary(uint value)
         {
-            Console.WriteLine(DecToBin(value));
+            Console.WriteLine($"Print Binary value of {value}: {DecToBin(value)}");
         }
         static bool IsPowerOfTwo(ulong x)
         {
